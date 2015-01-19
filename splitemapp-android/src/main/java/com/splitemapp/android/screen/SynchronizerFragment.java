b10246a160b2357cf5add6c61793cd6831b23151
Nicolas Albani <nicolasalbani@gmail.com>
2015-01-19 00:48:54 -0300

    Adding new push request task to the syncrhonizer fragment

--- splitemapp-android/src/main/java/com/splitemapp/android/screen/SynchronizerFragment.java
+++ splitemapp-android/src/main/java/com/splitemapp/android/screen/SynchronizerFragment.java
@@ -1,7 +1,9 @@
 package com.splitemapp.android.screen;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 import android.os.AsyncTask;
@@ -9,6 +11,7 @@ import android.util.Log;
 
 import com.j256.ormlite.dao.Dao.CreateOrUpdateStatus;
 import com.splitemapp.commons.constants.ServiceConstants;
+import com.splitemapp.commons.constants.TableField;
 import com.splitemapp.commons.constants.TableName;
 import com.splitemapp.commons.domain.ExpenseCategory;
 import com.splitemapp.commons.domain.Group;
@@ -35,15 +38,18 @@ import com.splitemapp.commons.domain.dto.UserInviteDTO;
 import com.splitemapp.commons.domain.dto.UserToGroupDTO;
 import com.splitemapp.commons.domain.dto.UserToProjectDTO;
 import com.splitemapp.commons.domain.dto.request.PullRequest;
+import com.splitemapp.commons.domain.dto.request.PushRequest;
 import com.splitemapp.commons.domain.dto.response.PullGroupResponse;
 import com.splitemapp.commons.domain.dto.response.PullProjectResponse;
 import com.splitemapp.commons.domain.dto.response.PullResponse;
 import com.splitemapp.commons.domain.dto.response.PullUserContactDataResponse;
-import com.splitemapp.commons.domain.dto.response.PullUserExpenseResult;
+import com.splitemapp.commons.domain.dto.response.PullUserExpenseResponse;
 import com.splitemapp.commons.domain.dto.response.PullUserInviteResponse;
 import com.splitemapp.commons.domain.dto.response.PullUserResponse;
 import com.splitemapp.commons.domain.dto.response.PullUserToGroupResponse;
 import com.splitemapp.commons.domain.dto.response.PullUserToProjectResponse;
+import com.splitemapp.commons.domain.dto.response.PushLongResponse;
+import com.splitemapp.commons.domain.dto.response.PushResponse;
 
 public abstract class SynchronizerFragment extends RestfulFragment{
 
@@ -101,6 +107,13 @@ public abstract class SynchronizerFragment extends RestfulFragment{
 	 */
 	protected void pullUserExpenses(){
 		new PullUserExpensesTask().execute();
+	}
+	
+	/**
+	 * Creates an asynchronous group table push request
+	 */
+	protected void pushGroups(){
+		new PushGroupsTask().execute();
 	}
 
 	/**
@@ -362,7 +375,7 @@ public abstract class SynchronizerFragment extends RestfulFragment{
 	 * Sync Task to pull user_expense table data from the remote DB
 	 * @author nicolas
 	 */
-	private class PullUserExpensesTask extends PullTask<UserExpenseDTO, PullUserExpenseResult> {
+	private class PullUserExpensesTask extends PullTask<UserExpenseDTO, PullUserExpenseResponse> {
 		@Override
 		protected String getTableName(){
 			return TableName.USER_EXPENSE;
@@ -374,7 +387,7 @@ public abstract class SynchronizerFragment extends RestfulFragment{
 		}
 
 		@Override
-		protected void processResult(PullUserExpenseResult response) throws SQLException {
+		protected void processResult(PullUserExpenseResponse response) throws SQLException {
 			Set<UserExpenseDTO> userExpenseDTOs = response.getItemSet();
 			for(UserExpenseDTO userExpenseDTO:userExpenseDTOs){
 				// We obtain the required parameters for the object creation from the local database
@@ -390,8 +403,8 @@ public abstract class SynchronizerFragment extends RestfulFragment{
 		}
 
 		@Override
-		protected Class<PullUserExpenseResult> getResponseType() {
-			return PullUserExpenseResult.class;
+		protected Class<PullUserExpenseResponse> getResponseType() {
+			return PullUserExpenseResponse.class;
 		}
 	}
 
@@ -433,19 +446,17 @@ public abstract class SynchronizerFragment extends RestfulFragment{
 		protected R doInBackground(Void... params) {
 			try {
 				// We get the date in which this table was last successfully pulled
-				//TODO remove the comment in the following line
-				//Date lastPullSuccessAt = getHelper().getSyncStatusDao().queryForEq(TableField.SYNC_STATUS_TABLE_NAME, getTableName()).get(0).getLastPullSuccessAt();
-				Date lastPullSuccessAt = new Date(100);
+				Date lastPullSuccessAt = getHelper().getSyncStatusDao().queryForEq(TableField.SYNC_STATUS_TABLE_NAME, getTableName()).get(0).getLastPullSuccessAt();
 
 				// We get the session token
 				String sessionToken = getHelper().getSessionToken();
 
-				// We create the login request
+				// We create the pull request
 				PullRequest pullRequest = new PullRequest();
 				pullRequest.setLastPullSuccessAt(lastPullSuccessAt);
 				pullRequest.setToken(sessionToken);
 
-				// We call the rest service and send back the login response
+				// We call the rest service and send back the pull response
 				return callRestService(getServicePath(), pullRequest, getResponseType());
 			} catch (Exception e) {
 				Log.e(getLoggingTag(), e.getMessage(), e);
@@ -474,6 +485,140 @@ public abstract class SynchronizerFragment extends RestfulFragment{
 					processResult(response);
 				} catch (SQLException e) {
 					Log.e(getLoggingTag(), "SQLException caught while processing " +pullMessage+ " response", e);
+				}
+
+				// We refresh the fragment we called the sync service from
+				refreshFragment();
+			}
+		}
+
+	}
+	
+	
+	/**
+	 * Sync Task to pull user_expense table data from the remote DB
+	 * @author nicolas
+	 */
+	private class PushGroupsTask extends PushTask<GroupDTO, Long, PushLongResponse> {
+		@Override
+		protected String getTableName(){
+			return TableName.GROUP;
+		}
+
+		@Override
+		protected String getServicePath(){
+			return ServiceConstants.PUSH_GROUPS_PATH;
+		}
+
+		@Override
+		protected Class<PushLongResponse> getResponseType() {
+			return PushLongResponse.class;
+		}
+
+		@Override
+		protected List<GroupDTO> getRequestItemList(Date lastPushSuccessAt) throws SQLException {
+			//TODO make one raw query that will return only the items updated after lastPushSuccessAt and add that in the DatabaseHelper 
+			// We get all the groups in the database
+			List<Group> groupList = getHelper().getGroupDao().queryForAll();
+			
+			// We add to the group DTO list the ones which were updated after the lastPushSuccessAt date 
+			ArrayList<GroupDTO> groupDTOList = new ArrayList<GroupDTO>();
+			for(Group group:groupList){
+				groupDTOList.add(new GroupDTO(group));
+			}
+			return groupDTOList;
+		}
+
+		@Override
+		protected void processResult(PushLongResponse response) throws SQLException {
+			//TODO still need to implement the updateRecordId method that will update the required id references
+		}
+	}
+	
+	/**
+	 * Base Push task
+	 * @author nicolas
+	 *
+	 * @param <E>
+	 */
+	private abstract class PushTask <F, E extends Number, R extends PushResponse<E>> extends AsyncTask<Void, Void, R> {
+
+		/**
+		 * Gets the name of the table to pull the data for
+		 * @return
+		 */
+		protected abstract String getTableName();
+
+		/**
+		 * Gets the path for the service to call
+		 * @return
+		 */
+		protected abstract String getServicePath();
+		
+		/**
+		 * Gets the response type
+		 * @return
+		 */
+		protected abstract Class<R> getResponseType();
+		
+		/**
+		 * Returns the request item list to be sent to the push service
+		 * @param lastPushSuccessAt Date containing the last push success date
+		 * @return List<F> containing the list of F objects to be sent to the push service
+		 * @throws SQLException
+		 */
+		protected abstract List<F> getRequestItemList(Date lastPushSuccessAt) throws SQLException;
+
+		/**
+		 * Processes the results coming from the service. This will typically contain DB inserts or updates
+		 * @param response ServiceResponse that contains the list returned by the server
+		 * @throws SQLException
+		 */
+		protected abstract void processResult(R response) throws SQLException;
+
+		@Override
+		protected R doInBackground(Void... params) {
+			try {
+				// We get the date in which this table was last successfully pulled
+				Date lastPushSuccessAt = getHelper().getSyncStatusDao().queryForEq(TableField.SYNC_STATUS_TABLE_NAME, getTableName()).get(0).getLastPushSuccessAt();
+				
+				// We get the session token
+				String sessionToken = getHelper().getSessionToken();
+				
+				// We create the push request
+				PushRequest<F> pushRequest = new PushRequest<F>();
+				pushRequest.setToken(sessionToken);
+				pushRequest.setItemList(getRequestItemList(lastPushSuccessAt));
+
+				// We call the rest service and send back the login response
+				return callRestService(getServicePath(), pushRequest, getResponseType());
+			} catch (Exception e) {
+				Log.e(getLoggingTag(), e.getMessage(), e);
+			}
+
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(R response) {
+			boolean pushSuccess = false;
+
+			// We validate the response
+			if(response != null){
+				pushSuccess = response.getSuccess();
+			}
+
+			// We show the status toast
+			String pushMessage = "Push " +getTableName();
+			showToast(pushSuccess ? pushMessage+ " Successful!" : pushMessage+ " Failed!");
+
+			// We save the user and session information returned by the backend
+			if(pushSuccess){
+				try {
+					// We process the service response
+					processResult(response);
+				} catch (SQLException e) {
+					Log.e(getLoggingTag(), "SQLException caught while processing " +pushMessage+ " response", e);
 				}
 
 				// We refresh the fragment we called the sync service from
