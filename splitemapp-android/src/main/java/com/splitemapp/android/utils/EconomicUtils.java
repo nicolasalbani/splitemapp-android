b3b1901a869eeecec6db0853dbb0b70f3ffa8013
Nicolas Albani <nicolasalbani@gmail.com>
2015-08-06 21:03:05 -0300

    Fixing problem when sending 0

--- splitemapp-android/src/main/java/com/splitemapp/android/utils/EconomicUtils.java
+++ splitemapp-android/src/main/java/com/splitemapp/android/utils/EconomicUtils.java
@@ -7,6 +7,9 @@ public class EconomicUtils {
 	public static final int MAX_SHARE = 100;
 
 	public static BigDecimal calulateShare(int peopleAmount){
+		if(peopleAmount<=0){
+			peopleAmount = 1;
+		}
 		return new BigDecimal(MAX_SHARE/peopleAmount);
 	}
 }
