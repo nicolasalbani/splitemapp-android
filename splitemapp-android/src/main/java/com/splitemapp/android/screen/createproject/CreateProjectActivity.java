4d9be7f4f4a2628dbf8f466c8ff6fc9a986530af
Nicolas Albani <nicolasalbani@gmail.com>
2015-08-10 20:53:54 -0300

    Fixing issue where when editing a project we pressed the back button, we would not clear the project id, hence next time we wanted to create one it would edit the previous

--- splitemapp-android/src/main/java/com/splitemapp/android/screen/createproject/CreateProjectActivity.java
+++ splitemapp-android/src/main/java/com/splitemapp/android/screen/createproject/CreateProjectActivity.java
@@ -1,11 +1,15 @@
 package com.splitemapp.android.screen.createproject;
 
 
+import java.util.ArrayList;
+
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 
+import com.splitemapp.android.constants.Globals;
 import com.splitemapp.android.screen.SingleFragmentActivity;
 import com.splitemapp.android.screen.home.HomeActivity;
+import com.splitemapp.commons.domain.User;
 
 public class CreateProjectActivity extends SingleFragmentActivity {
 
@@ -13,9 +17,15 @@ public class CreateProjectActivity extends SingleFragmentActivity {
 	protected Fragment createFragment() {
 		return new CreateProjectFragment();
 	}
-	
+
 	@Override
 	public void onBackPressed() {
+		// Resetting the global create project - user list
+		Globals.setCreateProjectActivityUserList(new ArrayList<User>());
+
+		// Resetting the global create project - project id
+		Globals.setCreateProjectActivityProjectId(null);
+
 		// We create an intent to the ProjectActivity sending the information from the clicked project
 		Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
 		startActivity(intent);
