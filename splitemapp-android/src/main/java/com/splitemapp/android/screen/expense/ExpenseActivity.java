7c8d34107a048bbfc81e07de843bef1ff2dffcb8
Nicolas Albani <nicolasalbani@gmail.com>
2015-08-11 16:19:33 -0300

    Fixing problems when clearing the expense ID

--- splitemapp-android/src/main/java/com/splitemapp/android/screen/expense/ExpenseActivity.java
+++ splitemapp-android/src/main/java/com/splitemapp/android/screen/expense/ExpenseActivity.java
@@ -3,6 +3,7 @@ package com.splitemapp.android.screen.expense;
 
 import android.support.v4.app.Fragment;
 
+import com.splitemapp.android.constants.Globals;
 import com.splitemapp.android.screen.SingleFragmentActivity;
 
 public class ExpenseActivity extends SingleFragmentActivity {
@@ -10,5 +11,14 @@ public class ExpenseActivity extends SingleFragmentActivity {
 	@Override
 	protected Fragment createFragment() {
 		return new ExpenseFragment();
+	}
+	
+	@Override
+	public void onBackPressed() {
+		// Cleaning up global expense id
+		Globals.setExpenseActivityExpenseId(null);
+
+		// Moving back to the project screen
+		finish();
 	}
 }
