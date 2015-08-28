28329495be0488212780fbfe92eb169144e35951
Nicolas Albani <nicolasalbani@gmail.com>
2015-08-27 21:59:38 -0300

    SPLITEMAPP-13 Adding missing shadow to the required fragments Removing unused imports from ExpenseFragment

--- splitemapp-android/src/main/java/com/splitemapp/android/screen/expense/ExpenseFragment.java
+++ splitemapp-android/src/main/java/com/splitemapp/android/screen/expense/ExpenseFragment.java
@@ -11,16 +11,13 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnKeyListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
