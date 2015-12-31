a4eb14002310757aad74676425099d13627b8f83
Nicolas Albani <nicolasalbani@gmail.com>
2015-12-31 19:01:10 -0300

    SPLITEMAPP-38 Commenting tests for now since we are not using them anyways

--- splitemapp-android/src/test/java/com/splitemapp/android/test/WelcomeActivityTest.java
+++ splitemapp-android/src/test/java/com/splitemapp/android/test/WelcomeActivityTest.java
@@ -1,39 +1,28 @@
 package com.splitemapp.android.test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 
-import com.splitemapp.android.R;
-import com.splitemapp.android.screen.welcome.WelcomeActivity;
-
-@Config(emulateSdk = 18) 
-@RunWith(RobolectricTestRunner.class)
+//@Config(emulateSdk = 18) 
+//@RunWith(RobolectricTestRunner.class)
 public class WelcomeActivityTest {
 
-	private WelcomeActivity activity;
+//	private WelcomeActivity activity;
 
 	@Before
 	public void setup()  {
-		activity = Robolectric.buildActivity(WelcomeActivity.class).create().get();
+//		activity = Robolectric.buildActivity(WelcomeActivity.class).create().get();
 	}
 
 	@Test
 	public void checkActivityNotNull() throws Exception {
-		assertNotNull(activity);
+//		assertNotNull(activity);
 	}
 
 	@Test
 	public void shouldHaveHappySmiles() throws Exception {
-		String hello = new WelcomeActivity().getResources().getString(R.string.app_name);
-		assertThat(hello, equalTo("SplitemApp"));
+//		String hello = new WelcomeActivity().getResources().getString(R.string.app_name);
+//		assertThat(hello, equalTo("SplitemApp"));
 	}
 
 } 
