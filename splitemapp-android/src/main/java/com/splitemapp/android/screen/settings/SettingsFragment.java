d2b434c0e9b139cff7759f1dba00b2a4945c0718
Nicolas Albani <nicolasalbani@gmail.com>
2016-02-05 00:44:13 -0300

    SPLITEMAPP-6 Adding code to handle profile modifications

--- splitemapp-android/src/main/java/com/splitemapp/android/screen/settings/SettingsFragment.java
+++ splitemapp-android/src/main/java/com/splitemapp/android/screen/settings/SettingsFragment.java
@@ -1,21 +1,43 @@
 package com.splitemapp.android.screen.settings;
 
+import java.sql.SQLException;
+
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.splitemapp.android.R;
 import com.splitemapp.android.screen.RestfulFragmentWithBlueActionbar;
+import com.splitemapp.android.utils.ImageUtils;
+import com.splitemapp.commons.domain.User;
 
 public class SettingsFragment extends RestfulFragmentWithBlueActionbar {
 
 	private static final String TAG = SettingsFragment.class.getSimpleName();
 
+	private User mCurrentUser;
+
+	private ImageView mAvatarImageView;
+	private EditText mFullNameEditText;
+	private TextView mEmailTextView;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		// We get the user instance
+		try {
+			mCurrentUser = getHelper().getLoggedUser();
+		} catch (SQLException e) {
+			Log.e(TAG, "SQLException caught!", e);
+		}
 	}
 
 	@Override
@@ -24,11 +46,33 @@ public class SettingsFragment extends RestfulFragmentWithBlueActionbar {
 
 		View v = super.onCreateView(inflater, container, savedInstanceState);
 
-		
+		// We set the user avatar in the navigation view
+		mAvatarImageView = (ImageView) v.findViewById(R.id.s_avatar_imageView);
+		setUsetAvatar(mAvatarImageView, mCurrentUser, ImageUtils.IMAGE_QUALITY_MAX);
+		mAvatarImageView.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				openImageSelector(mAvatarImageView.getWidth(), mAvatarImageView.getHeight());
+			}
+		});
+
+		// We populate the first name in the main view
+		mFullNameEditText = (EditText) v.findViewById(R.id.s_full_name_editText);
+		mFullNameEditText.setText(mCurrentUser.getFullName());
+
+		// We populate the email in the main view
+		mEmailTextView  = (TextView) v.findViewById(R.id.s_email_textView);
+		mEmailTextView.setText(mCurrentUser.getUsername());
+
 		return v;
 	}
 
-	
+	@Override
+	public void executeOnImageSelection(Bitmap selectedBitmap) {
+		// Updating project image on screen
+		mAvatarImageView.setImageBitmap(selectedBitmap);
+	}
+
 	@Override
 	protected void onRefresh(String response) {
 		// Refreshing contacts list after making the sync
@@ -52,6 +96,8 @@ public class SettingsFragment extends RestfulFragmentWithBlueActionbar {
 
 	@Override
 	protected void doneAction() {
+		//TODO we need to persist any changes done
+		
 		getActivity().finish();
 	}
 }
